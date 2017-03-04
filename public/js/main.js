4b817989e2c5bef328abbc94ba174ce860e8d9f8
Michael Kelly <mike@whatsthatweb.com.au>
2017-03-04 15:26:12 +1100

    Range styled and nice

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
 
-  $('input[type="range"]').rangeslider();
+  var $element = $('input[type="range"]');
+  var $handle;
+  var $ruler = $('<div class="rangeslider__ruler" />');
+
+  $element
+    .rangeslider({
+      polyfill: false,
+      onInit: function() {
+
+        // handle
+        $handle = $('.rangeslider__handle', this.$range);
+        updateHandle($handle[0], this.value);
+
+        // ruler
+        $ruler[0].innerHTML = getRulerRange(this.min, this.max, this.step);
+        this.$range.prepend($ruler);
+      }
+    })
+    .on('input', function() {
+      updateHandle($handle[0], this.value);
+    });
+
+  function updateHandle(el, val) {
+    el.textContent = val;
+  }
+
+  function getRulerRange(min, max, step) {
+    var range = '';
+    var i = min;
+
+    while (i <= max) {
+      range += i + ' ';
+      i = i + step;
+    }
+    return range;
+  }
 
 });
